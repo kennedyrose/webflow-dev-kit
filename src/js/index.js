b1c33657a702cae58601f5830d636056b93bbd4f
Kennedy Rose <kennedyianrose@gmail.com>
2023-04-06 16:21:55 -0500

    More form methods

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,13 @@
-import hijackForm from "./hijack-form"
+import WebflowForm from "./webflow-form"
 
-function isDev(){
-	try{
-		if(import.meta.env.MODE === 'development'){
-			return true
-		}
-	}
-	catch(err){}
-	return false
-}
+const form = new WebflowForm(`#wf-form-Contact`, async () => {
+	form.disable().showWait()
 
 
+	const data = form.getValues()
+	console.log(`data`, data)
 
-hijackForm({
-	form: '#wf-form-Contact',
-	onSubmit: async (data) => {
-		console.log(`data`, data)
-	},
+	
+
+	form.enable().hideWait()
 })
-
-
-// let apiRoute = import.meta.env.VITE_API
-// console.log(`apiRoute`, apiRoute)
-// apiRoute = apiRoute.replace(/\/$/, '')
-
-// async function fetchData(){
-// 	const response = await fetch(`${apiRoute}/hello`)
-// 	const data = await response.json()
-// 	console.log(data)
-// }
-// fetchData()
-// 	.catch(err => console.error(err))
