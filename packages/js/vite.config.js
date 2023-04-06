d143e4b71b910a3aad9ecdfce5e89a4f3394bf66
Kennedy Rose <kennedyianrose@gmail.com>
2023-04-06 09:12:41 -0500

    Local JS

--- packages/js/vite.config.js
+++ packages/js/vite.config.js
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
+	root: `src`,
 	server: {
 		port: 5173,
+		outDir: '../dist',
+		lib: {
+		  entry: path.resolve(__dirname, 'src/index.js'),
+		  name: 'WebflowDev',
+		  fileName: (format) => `webflowdev.${format}.js`
+		},
+	},
+	build: {
+		outDir: '../dist',
+		lib: {
+		  entry: path.resolve(__dirname, 'src/index.js'),
+		  name: 'WebflowDev',
+		  fileName: (format) => `webflowdev.${format}.js`
+		},
+		// watch: {
+		// 	include: `src/**/*`,
+		// },
 	},
 })
+
+
