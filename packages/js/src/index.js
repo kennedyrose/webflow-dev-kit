1d6e432217d24923c3e7be93a39f040f9e512757
Kennedy Rose <kennedyianrose@gmail.com>
2023-04-06 09:43:33 -0500

    Automatically set API endpoint

--- packages/js/src/index.js
+++ packages/js/src/index.js
@@ -1,1 +1,10 @@
-console.log(`test`)
+let apiRoute = import.meta.env.VITE_API
+apiRoute = apiRoute.replace(/\/$/, '')
+
+async function fetchData(){
+	const response = await fetch(`${apiRoute}/hello`)
+	const data = await response.json()
+	console.log(data)
+}
+fetchData()
+	.catch(err => console.error(err))
